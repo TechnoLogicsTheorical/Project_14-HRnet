3bcd6d00e0886e3b5673ae733c061b88afa5483a
Anthony.M <marcellin.anthony77@gmail.com>
2023-03-30 19:23:59 +0200

    Creating page with generics components and associed components

---
+++ src/Layouts/ListEmployee.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from '../Components/Generics/Header/index.jsx';
+
+export default function ListEmployee () {
+    return (
+        <>
+        </>
+    )
+}
