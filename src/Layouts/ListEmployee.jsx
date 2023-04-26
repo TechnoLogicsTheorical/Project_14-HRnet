43a7451c7145a74cb9ec535cb602f2984efb009b
Anthony.M <marcellin.anthony77@gmail.com>
2023-04-26 22:45:54 +0200

    Add Search functionality

--- src/Layouts/ListEmployee.jsx
+++ src/Layouts/ListEmployee.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+import filteredEmployeesList from '../utils/Functions/filteredEmployee.js';
 
 import Title from '../Components/Generics/Title';
 import EmployeeTable from '../Components/Specifik/EmployeeTable/index.jsx';
 import Header from '../Components/Generics/Header/index.jsx';
+
+const FormInput = styled.input`
+  display: inline-block;
+  font-size: inherit;
+  padding: 7px 12px;
+  margin: 0 12px;
+  border-radius: 5px;
+  border: 1px solid black;
+`;
 
 /**
  * Composant de page permettant d'accéder à la fonctionnalité d'affichage de tous les employés
@@ -12,11 +24,30 @@ import Header from '../Components/Generics/Header/index.jsx';
  */
 export default function ListEmployee () {
     const employees = useSelector((state) => state.employee);
+    const [employeeList, setEmployeeList] = useState(employees);
+
+    const handleResearch = (event) => {
+        const value = event.target.value;
+
+        const newEmployeeList = filteredEmployeesList(value, employees);
+        setEmployeeList(newEmployeeList);
+    }
     return (
         <>
             <Header />
             <Title name='All Employees' level={4} />
-            <EmployeeTable employeesList={employees} />
+
+            <div>
+                <label htmlFor="searchText">Recherche</label>
+                <FormInput
+                    id="searchText"
+                    type="text"
+                    onChange={handleResearch}
+                    placeholder="FirstName or LastName"
+                />
+            </div>
+
+            <EmployeeTable employeesList={employeeList} />
         </>
     )
 }
