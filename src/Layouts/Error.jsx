1369969cda5293ac2bd2968437dad79abe6e40bb
Anthony.M <marcellin.anthony77@gmail.com>
2023-04-26 21:00:04 +0200

    Reorganisation of code

--- src/Layouts/Error.jsx
+++ src/Layouts/Error.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Title from '../Components/Generics/Title';
+import { NavLink } from 'react-router-dom';
 
+/**
+ * Composant page en cas d'une mauvaise URL saisie ou aucune page ne pouvant correspondre aux déclarations du routeur
+ * @returns {JSX.Element} Les éléments HTML nécessaires
+ * @constructor
+ */
 export default function Error() {
     return (
         <>
-            <Title name='Error' />
+            <Title level={4} name='Error' />
+            <br />
+            <br />
+            <NavLink to={'/'}>Retourner à la page d'accueil</NavLink>
         </>
     )
 }
